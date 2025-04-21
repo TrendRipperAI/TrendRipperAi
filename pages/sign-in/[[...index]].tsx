e7aeb633d507fde96818c8d8e43238d20d77301c
Kyle Bekker <kyle@trendripperai.com>
2025-04-21 12:01:35 +0200

    💥 Swapped dynamic Clerk routes for flat ones to fix Vercel error

--- pages/sign-in/[[...index]].tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '100px' }}>
-      <SignIn routing="path" signUpUrl="/sign-up" />
-    </div>
-  );
-}
