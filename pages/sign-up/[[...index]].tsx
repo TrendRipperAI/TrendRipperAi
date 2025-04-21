4fc94723725dca5e13e89a781b051e4c84425cd2
Kyle Bekker <kyle@trendripperai.com>
2025-04-21 11:50:56 +0200

    🐛 Fix: Add missing path props to Clerk sign-in/sign-up

--- pages/sign-up/[[...index]].tsx
+++ pages/sign-up/[[...index]].tsx
@@ -3,7 +3,8 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '100px' }}>
-      <SignUp routing="path" signInUrl="/sign-in" />
+      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+
     </div>
   );
 }
