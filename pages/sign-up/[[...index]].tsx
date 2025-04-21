dbd4c7d8206519ac52f22028c66ec15df47ce820
Kyle Bekker <kyle@trendripperai.com>
2025-04-21 11:53:54 +0200

    ✅ Fix: add missing Clerk path props for Vercel deploy

--- pages/sign-up/[[...index]].tsx
+++ pages/sign-up/[[...index]].tsx
@@ -4,7 +4,6 @@ export default function SignUpPage() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '100px' }}>
       <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-
     </div>
   );
 }
